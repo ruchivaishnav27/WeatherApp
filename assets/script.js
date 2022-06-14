adff4d1f6b592b99194259ad06e730b11a677222
Ruchi Vaishnav <ruchivaishnav27@gmail.com>
2022-06-14 18:05:58 -0400

    Added API

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,66 @@
-var userFormEl = document.querySelector("#user-form");
-var languageButtonsEl = document.querySelector("#language-buttons");
-var desiredCityEl = document.querySelector("#desiredcity");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
+var cityFormEl = document.querySelector("#city-form");
+var cityNameEl = document.querySelector("#city-name");
+var nameInputEl = document.querySelector("#cityname");
+var cityContainerEl = document.querySelector("#city-container");
+var citySearch = document.querySelector("#city-search");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
 
   // get value from input element
-  var username = nameInputEl.value.trim();
+  var cityname = nameInputEl.value.trim();
 
-  if (username) {
-    getUserRepos(username);
+  if (cityname) {
+    getCityName(cityname);
 
     // clear old content
-    repoContainerEl.textContent = "";
+    cityContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
-    alert("Please enter a GitHub username");
+    alert("Search for a city or select from the following");
   }
 };
 
 var buttonClickHandler = function(event) {
   // get the language attribute from the clicked element
-  var language = event.target.getAttribute("data-language");
+  var city = event.target.getAttribute("city-name");
 
-  if (language) {
-    getFeaturedRepos(language);
+  if (city) {
+    getFeaturedCities(city);
 
     // clear old content
-    repoContainerEl.textContent = "";
+    cityContainerEl.textContent = "";
   }
 };
 
-var getUserRepos = function(user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
+var getCityName = function(city) {
   // make a get request to url
-  fetch(apiUrl)
-    .then(function(response) {
+  fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={a5f0248fee7817e0ec325587c9e1160b}").then(function(response) {
       // request was successful
       if (response.ok) {
         console.log(response);
         response.json().then(function(data) {
           console.log(data);
-          displayRepos(data, user);
+          displayCities(data, city);
         });
       } else {
-        alert('Error: GitHub User Not Found');
+        alert('Error: City does not exist');
       }
     })
     .catch(function(error) {
-      alert("Unable to connect to GitHub");
+      alert("City cannot be found");
     });
 };
 
-var getFeaturedRepos = function(language) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+var getFeaturedCities = function(city) {
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+  fetch("https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={a5f0248fee7817e0ec325587c9e1160b}").then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        displayRepos(data.items, language);
+        displayCities(data.items, city);
       });
     } else {
       alert("Error: " + response.statusText);
@@ -76,53 +70,23 @@ var getFeaturedRepos = function(language) {
 
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
-  if (repos.length === 0) {
-    repoContainerEl.textContent = "No repositories found.";
+  if (city.length === 0) {
+    cityContainerEl.textContent = "No cites to display.";
     return;
   }
 
-  repoSearchTerm.textContent = searchTerm;
-
-  // loop over repos
-  for (var i = 0; i < repos.length; i++) {
-    // format repo name
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
-
-    // create a link for each repo
-    var repoEl = document.createElement("a");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
-
-    // create a span element to hold repository name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
+  citySearch.textContent = searchTerm;
 
     // append to container
-    repoEl.appendChild(titleEl);
-
-    // create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
-
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    // append to container
-    repoEl.appendChild(statusEl);
+    cityEl.appendChild(statusEl);
 
     // append container to the dom
-    repoContainerEl.appendChild(repoEl);
-  }
-};
+    cityContainerEl.appendChild(repoEl);
+  };
 
 // add event listeners to form and button container
-userFormEl.addEventListener("submit", formSubmitHandler);
-languageButtonsEl.addEventListener("click", buttonClickHandler);
+cityFormEl.addEventListener("submit", formSubmitHandler);
+cityNameEl.addEventListener("click", buttonClickHandler);
 
 
 
